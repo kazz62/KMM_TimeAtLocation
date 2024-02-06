78d8d6978039bc05046f7e3476e624dad38efe8a
kazz62 <72215628+kazz62@users.noreply.github.com>
2024-02-06 12:08:44 +0100

    feature_ktor_init
    Ktor initialization and Api request working on Android & iOS.
    
    Still wip for desktop

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -2,6 +2,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -9,12 +10,15 @@ import androidx.compose.material.Button
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,6 +26,10 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.IllegalTimeZoneException
 import kotlinx.datetime.LocalDateTime
@@ -45,7 +53,7 @@ fun App(countries: List<Country> = countries()) {
                 textAlign = TextAlign.Center,
                 modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
             )
-            Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)){
+            Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)) {
                 DropdownMenu(
                     expanded = showCountries,
                     onDismissRequest = { showCountries = false }
@@ -60,7 +68,7 @@ fun App(countries: List<Country> = countries()) {
                             Row(verticalAlignment = Alignment.CenterVertically) {
                                 Image(
                                     painterResource(image),
-                                    modifier = Modifier.size(50.dp).padding(end= 10.dp),
+                                    modifier = Modifier.size(50.dp).padding(end = 10.dp),
                                     contentDescription = "$name flag"
                                 )
                                 Text(name)
@@ -69,15 +77,32 @@ fun App(countries: List<Country> = countries()) {
                     }
                 }
             }
-            Button(modifier = Modifier.padding(start= 20.dp, top = 10.dp),
-                onClick = { showCountries = !showCountries}) {
+            Button(modifier = Modifier.padding(start = 20.dp, top = 10.dp),
+                onClick = { showCountries = !showCountries }) {
                 Text("Show Time At Location")
             }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                val scope = rememberCoroutineScope()
+                var text by remember { mutableStateOf("Loading") }
+                LaunchedEffect(true) {
+                    scope.launch {
+                        text = try {
+                            Greeting().greeting()
+                        } catch (e: Exception) {
+                            e.message ?: "error"
+                        }
+                    }
+                }
+                GreetingView(text)
+            }
+
         }
     }
 }
 
-fun currentTimeAt(location: String, zone: TimeZone) : String {
+fun currentTimeAt(location: String, zone: TimeZone): String {
     fun LocalTime.formatted() = "$hour:$minute:$second"
 
     val time = Clock.System.now()
@@ -101,3 +126,8 @@ fun countries() = listOf(
     Country("Indonesia", TimeZone.of("Asia/Jakarta"), "id.png"),
     Country("Egypt", TimeZone.of("Africa/Cairo"), "eg.png"),
 )
+
+@Composable
+fun GreetingView(text: String) {
+    Text(text = text)
+}
